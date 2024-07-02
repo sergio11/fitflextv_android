4a1b280043ce1f86b5cf0e277a0ce8bfb82605db
Sergio Sánchez <sss4esob@gmail.com>
2024-07-02 21:01:58 +0200

    Add firebase dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,18 @@
     which will be fixed with Gradle 8.1 or later.
     https://youtrack.jetbrains.com/issue/KTIJ-19369/
  */
+
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
 plugins {
     alias(libs.plugins.androidApplication) apply(false)
     alias(libs.plugins.kotlinAndroid) apply(false)
     alias(libs.plugins.kotlin.serialization) apply(false)
     alias(libs.plugins.hilt) apply(false)
     alias(libs.plugins.ksp) apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
     alias(libs.plugins.androidTest) apply false
 }
