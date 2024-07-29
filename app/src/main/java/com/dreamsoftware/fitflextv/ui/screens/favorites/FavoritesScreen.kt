b3aaa85867622cdcfb00679639edad8042af933d
Sergio Sánchez <sss4esob@gmail.com>
2024-07-29 19:55:44 +0200

    open training program from favorites screen

--- app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/favorites/FavoritesScreen.kt
@@ -2,20 +2,23 @@ package com.dreamsoftware.fitflextv.ui.screens.favorites
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dreamsoftware.fitflextv.domain.model.TrainingTypeEnum
 import com.dreamsoftware.fitflextv.ui.core.components.CommonScreen
 
 @Composable
 fun FavoritesScreen(
     viewModel: FavoritesViewModel = hiltViewModel(),
     onBackPressed: () -> Unit,
-    onStartWorkout: (id: String) -> Unit
+    onOpenTrainingProgramDetail: (id: String, type: TrainingTypeEnum) -> Unit
 ) {
     CommonScreen(
         viewModel = viewModel,
         onBackPressed = onBackPressed,
         onInitialUiState = { FavoritesUiState() },
         onSideEffect = {
-
+            when(it) {
+                is FavoritesSideEffects.OpenTrainingProgramDetail -> onOpenTrainingProgramDetail(it.id, it.type)
+            }
         },
         onInit = {
             fetchData()
