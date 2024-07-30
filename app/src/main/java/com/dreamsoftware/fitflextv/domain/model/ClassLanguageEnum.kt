223a80daffadbdd3a6ff2bb52719d18c8734adba
Sergio Sánchez <sss4esob@gmail.com>
2024-07-30 20:11:34 +0200

    Implement filter side menu in training screen

---
+++ app/src/main/java/com/dreamsoftware/fitflextv/domain/model/ClassLanguageEnum.kt
@@ -0,0 +1,13 @@
+package com.dreamsoftware.fitflextv.domain.model
+
+enum class ClassLanguageEnum(val value: String) {
+    ENGLISH("English"),
+    SPANISH("Spanish"),
+    FRENCH("French"),
+    GERMAN("German"),
+    ITALIAN("Italian"),
+    RUSSIAN("Russian"),
+    JAPANESE("Japanese"),
+    CHINESE("Chinese"),
+    KOREAN("Korean")
+}
---
+++ app/src/main/java/com/dreamsoftware/fitflextv/domain/model/DifficultyEnum.kt
@@ -0,0 +1,5 @@
+package com.dreamsoftware.fitflextv.domain.model
+
+enum class DifficultyEnum(val value: String) {
+    BEGINNER("Beginner"), INTERMEDIATE("Intermediate"), ADVANCED("Advanced")
+}
