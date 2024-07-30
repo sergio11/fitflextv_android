9f5954c047c5cb6091c33a411c47deafd2785c5c
Sergio Sánchez <sss4esob@gmail.com>
2024-07-30 19:57:48 +0200

    Implement filter side menu in training screen

--- app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/training/components/OptionsSideMenu.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/training/components/OptionsSideMenu.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,6 +24,8 @@ import androidx.tv.material3.RadioButtonDefaults
 import androidx.tv.material3.Text
 import com.dreamsoftware.fitflextv.R
 import com.dreamsoftware.fitflextv.ui.core.components.CommonFillButton
+import com.dreamsoftware.fitflextv.ui.core.components.CommonFocusRequester
+import com.dreamsoftware.fitflextv.ui.utils.conditional
 
 @Composable
 fun OptionsSideMenu(
@@ -33,57 +36,63 @@ fun OptionsSideMenu(
     onSelectedItem: (currentIndex: Int) -> Unit,
 ) {
     with(MaterialTheme.colorScheme) {
-        TvLazyColumn(
-            contentPadding = PaddingValues(vertical = 32.dp, horizontal = 20.dp)
-        ) {
-            item {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 24.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(titleRes),
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = onSurface
-                    )
-                    CommonFillButton(
-                        text = stringResource(R.string.reset),
-                        onClick = onDismissSideMenu
+        CommonFocusRequester { focusRequester ->
+            TvLazyColumn(
+                contentPadding = PaddingValues(vertical = 32.dp, horizontal = 20.dp)
+            ) {
+                item {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 24.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = stringResource(titleRes),
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = onSurface
+                        )
+                        CommonFillButton(
+                            text = stringResource(R.string.reset),
+                            onClick = onDismissSideMenu
+                        )
+                    }
+                }
+                items(items.size) { index ->
+                    ListItem(
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                            .conditional(condition = index == 0, ifTrue = {
+                                focusRequester(focusRequester)
+                            }),
+                        selected = false,
+                        onClick = { onSelectedItem(index) },
+                        trailingContent = {
+                            RadioButton(
+                                selected = selectedIndex == index,
+                                onClick = { onSelectedItem(index) },
+                                modifier = Modifier.size(ListItemDefaults.IconSizeDense),
+                                colors = RadioButtonDefaults.colors(
+                                    selectedColor = inversePrimary,
+                                    unselectedColor = border
+                                )
+                            )
+                        },
+                        headlineContent = {
+                            Text(
+                                text = items[index],
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        },
+                        colors = ListItemDefaults.colors(
+                            containerColor = Color.Transparent,
+                            contentColor = onSurfaceVariant,
+                            focusedContentColor = surfaceVariant
+                        ),
+                        shape = ListItemDefaults.shape(shape = RoundedCornerShape(16.dp))
                     )
                 }
-            }
-            items(items.size) { index ->
-                ListItem(
-                    modifier = Modifier.padding(top = 16.dp),
-                    selected = false,
-                    onClick = { onSelectedItem(index) },
-                    trailingContent = {
-                        RadioButton(
-                            selected = selectedIndex == index,
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier.size(ListItemDefaults.IconSizeDense),
-                            colors = RadioButtonDefaults.colors(
-                                selectedColor = inversePrimary,
-                                unselectedColor = border
-                            )
-                        )
-                    },
-                    headlineContent = {
-                        Text(
-                            text = items[index],
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                    },
-                    colors = ListItemDefaults.colors(
-                        containerColor = Color.Transparent,
-                        contentColor = onSurfaceVariant,
-                        focusedContentColor = surfaceVariant
-                    ),
-                    shape = ListItemDefaults.shape(shape = RoundedCornerShape(16.dp))
-                )
             }
         }
     }
