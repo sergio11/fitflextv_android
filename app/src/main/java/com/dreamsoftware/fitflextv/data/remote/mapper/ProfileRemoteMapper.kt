8d94bba87edccf2bf936780604e59fc94ebaff8f
Sergio Sánchez <sss4esob@gmail.com>
2024-08-03 11:02:12 +0200

    Remove isAdmin field

--- app/src/main/java/com/dreamsoftware/fitflextv/data/remote/mapper/ProfileRemoteMapper.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/data/remote/mapper/ProfileRemoteMapper.kt
@@ -8,7 +8,6 @@ internal class ProfileRemoteMapper : IOneSideMapper<Map<String, Any?>, ProfileDT
     private companion object {
         const val UUID_KEY = "uuid"
         const val ALIAS_KEY = "alias"
-        const val IS_ADMIN_KEY = "isAdmin"
         const val IS_SECURED_KEY = "isSecured"
         const val USER_ID_KEY = "userId"
         const val AVATAR_TYPE_KEY = "avatarType"
@@ -18,7 +17,6 @@ internal class ProfileRemoteMapper : IOneSideMapper<Map<String, Any?>, ProfileDT
         ProfileDTO(
             uuid = get(UUID_KEY) as String,
             alias = get(ALIAS_KEY) as String,
-            isAdmin = get(IS_ADMIN_KEY) as Boolean,
             isSecured = get(IS_SECURED_KEY) as Boolean,
             avatarType = get(AVATAR_TYPE_KEY) as String,
             userId = get(USER_ID_KEY) as String
--- app/src/main/java/com/dreamsoftware/fitflextv/domain/model/ProfileBO.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/domain/model/ProfileBO.kt
@@ -3,7 +3,6 @@ package com.dreamsoftware.fitflextv.domain.model
 data class ProfileBO(
     val uuid: String,
     val alias: String,
-    val isAdmin: Boolean,
     val isSecured: Boolean,
     val avatarType: AvatarTypeEnum
 )
