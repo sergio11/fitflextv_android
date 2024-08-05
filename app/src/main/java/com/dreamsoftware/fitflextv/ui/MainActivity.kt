7c881eda45af4985a4e25129f5ebd758a5db6e70
Sergio Sánchez <sss4esob@gmail.com>
2024-08-05 18:08:14 +0200

    Implement screen state receiver

--- app/src/main/java/com/dreamsoftware/fitflextv/ui/MainActivity.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/MainActivity.kt
@@ -5,17 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import com.dreamsoftware.fitflextv.ui.receiver.ScreenStateReceiver
 import com.dreamsoftware.fitflextv.ui.screens.app.AppScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private lateinit var screenStateReceiver: ScreenStateReceiver
+
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        screenStateReceiver = ScreenStateReceiver.register(this)
         setContent {
             AppScreen()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ScreenStateReceiver.unregister(this, screenStateReceiver)
     }
 }
---
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/receiver/ScreenStateReceiver.kt
@@ -0,0 +1,47 @@
+package com.dreamsoftware.fitflextv.ui.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+import com.dreamsoftware.fitflextv.utils.AppEvent
+import com.dreamsoftware.fitflextv.utils.AppEventBus
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ScreenStateReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var appEventBus: AppEventBus
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_SCREEN_ON -> {
+                Log.d("ScreenStateReceiver", "ACTION_SCREEN_ON")
+                appEventBus.send(AppEvent.ComeFromStandby)
+            }
+            Intent.ACTION_SCREEN_OFF -> {
+                Log.d("ScreenStateReceiver", "ACTION_SCREEN_OFF")
+                appEventBus.send(AppEvent.GoToStandby)
+            }
+        }
+    }
+
+    companion object {
+        fun register(context: Context): ScreenStateReceiver {
+            val receiver = ScreenStateReceiver()
+            val filter = IntentFilter().apply {
+                addAction(Intent.ACTION_SCREEN_ON)
+                addAction(Intent.ACTION_SCREEN_OFF)
+            }
+            context.registerReceiver(receiver, filter)
+            return receiver
+        }
+
+        fun unregister(context: Context, receiver: ScreenStateReceiver) {
+            context.unregisterReceiver(receiver)
+        }
+    }
+}
