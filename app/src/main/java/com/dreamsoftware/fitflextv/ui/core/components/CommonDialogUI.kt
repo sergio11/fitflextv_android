01338584cd3e82c8e4e608a91e54e0aa570cdfc4
Sergio Sánchez <sss4esob@gmail.com>
2024-08-05 18:04:13 +0200

    Implement several improvements in CommonDialogUI

--- app/src/main/java/com/dreamsoftware/fitflextv/ui/core/components/CommonDialogUI.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/core/components/CommonDialogUI.kt
@@ -3,33 +3,29 @@ package com.dreamsoftware.fitflextv.ui.core.components
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.tv.material3.Card
-import androidx.tv.material3.CardDefaults
-import androidx.tv.material3.CardShape
 import androidx.tv.material3.MaterialTheme
 import com.dreamsoftware.fitflextv.R
 
@@ -80,11 +76,11 @@ private fun CommonDialogUI(
     customContent: @Composable ColumnScope.() -> Unit = {}
 ) {
     with(MaterialTheme.colorScheme) {
-        Card(
-            onClick = { },
+        Surface(
             modifier = modifier
-                .background(Color.Transparent, RoundedCornerShape(16.dp))
-                .padding(bottom = 8.dp),
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Transparent)
+                .border(2.dp, Color.Gray, RoundedCornerShape(16.dp)),
         ) {
             Column(
                 modifier = modifier.background(inverseSurface),
@@ -126,8 +122,7 @@ private fun CommonDialogUI(
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .background(background.copy(0.2F)),
+                        .padding(vertical = 8.dp),
                     horizontalArrangement = Arrangement.Center
                 ) {
                     onCancelClicked?.let {
