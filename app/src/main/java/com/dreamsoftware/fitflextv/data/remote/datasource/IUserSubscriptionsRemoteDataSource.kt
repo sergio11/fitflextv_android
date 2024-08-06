9800762defe556c290a008c883a78088279ef0da
Sergio Sánchez <sss4esob@gmail.com>
2024-08-06 19:57:32 +0200

    Implement user subscriptions remote data source

--- app/src/main/java/com/dreamsoftware/fitflextv/data/remote/datasource/IUserSubscriptionsRemoteDataSource.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/data/remote/datasource/IUserSubscriptionsRemoteDataSource.kt
@@ -5,11 +5,15 @@ import com.dreamsoftware.fitflextv.data.remote.dto.response.UserSubscriptionDTO
 import com.dreamsoftware.fitflextv.data.remote.exception.AddUserSubscriptionRemoteException
 import com.dreamsoftware.fitflextv.data.remote.exception.FetchUserSubscriptionRemoteException
 import com.dreamsoftware.fitflextv.data.remote.exception.RemoveUserSubscriptionRemoteException
+import com.dreamsoftware.fitflextv.data.remote.exception.VerifyHasActiveSubscriptionRemoteException
 
 interface IUserSubscriptionsRemoteDataSource {
 
     @Throws(FetchUserSubscriptionRemoteException::class)
     suspend fun getUserSubscription(userId: String): UserSubscriptionDTO
+
+    @Throws(VerifyHasActiveSubscriptionRemoteException::class)
+    suspend fun hasActiveSubscription(userId: String): Boolean
 
     @Throws(AddUserSubscriptionRemoteException::class)
     suspend fun addUserSubscription(data: AddUserSubscriptionDTO): Boolean
