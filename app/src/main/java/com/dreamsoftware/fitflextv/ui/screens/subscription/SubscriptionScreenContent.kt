6afabda619e4dcbe7b13b501565ccbc2a4b69c2c
Sergio Sánchez <sss4esob@gmail.com>
2024-09-18 19:56:48 +0200

    Update libraries version

--- app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/subscription/SubscriptionScreenContent.kt
+++ app/src/main/java/com/dreamsoftware/fitflextv/ui/screens/subscription/SubscriptionScreenContent.kt
@@ -1,35 +1,17 @@
 package com.dreamsoftware.fitflextv.ui.screens.subscription
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import androidx.tv.material3.MaterialTheme
 import com.dreamsoftware.fitflextv.R
-import com.dreamsoftware.fitflextv.ui.screens.subscription.components.SubscriptionHeadline
 import com.dreamsoftware.fitflextv.ui.screens.subscription.components.SubscriptionOptions
 import com.dreamsoftware.fudge.component.FudgeTvButton
 import com.dreamsoftware.fudge.component.FudgeTvButtonStyleTypeEnum
 import com.dreamsoftware.fudge.component.FudgeTvButtonTypeEnum
 import com.dreamsoftware.fudge.component.FudgeTvDialog
+import com.dreamsoftware.fudge.component.FudgeTvPreviewContent
 import com.dreamsoftware.fudge.component.FudgeTvScreenContent
 
 @Composable
@@ -46,57 +28,36 @@ fun SubscriptionScreenContent(
                 descriptionRes = R.string.add_subscription_completed_dialog_description,
                 onAcceptClicked = actionListener::onCompleted
             )
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .semantics { contentDescription = "Subscription Screen" },
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                Image(
-                    modifier = Modifier
-                        .size(width = 324.dp, height = 452.dp)
-                        .border(
-                            border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
-                            shape = RoundedCornerShape(16.dp)
-                        )
-                        .clip(RoundedCornerShape(16.dp)),
-                    painter = painterResource(id = R.drawable.signup_background),
-                    contentDescription = "training photo",
-                    contentScale = ContentScale.Crop
-                )
-                Spacer(modifier = Modifier.width(28.dp))
-                Column(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    verticalArrangement = Arrangement.spacedBy(32.dp),
-                ) {
-                    SubscriptionHeadline()
+            FudgeTvPreviewContent(
+                imageRes = R.drawable.signup_background,
+                defaultImagePlaceholderRes = R.drawable.main_logo,
+                mainTitleRes = R.string.subscribe_to_premium,
+                secondaryTitleRes = R.string.subscribe_brief,
+                onBuildContent = {
                     SubscriptionOptions(
                         subscriptionOptions = subscriptionList,
                         selectedSubscription = selectedSubscription,
                         updateSelectedSubscriptionOption = actionListener::onSubscriptionOptionUpdated
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceAround
-                    ) {
+                },
+                onBuildActions = {
+                    FudgeTvButton(
+                        type = FudgeTvButtonTypeEnum.LARGE,
+                        style = FudgeTvButtonStyleTypeEnum.NORMAL,
+                        textRes = R.string.subscribe_now,
+                        onClick = actionListener::onSubscribe
+                    )
+                    if(hasActiveSubscription) {
+                        Spacer(modifier = Modifier.width(10.dp))
                         FudgeTvButton(
                             type = FudgeTvButtonTypeEnum.LARGE,
-                            style = FudgeTvButtonStyleTypeEnum.NORMAL,
-                            textRes = R.string.subscribe_now,
-                            onClick = actionListener::onSubscribe
+                            style = FudgeTvButtonStyleTypeEnum.INVERSE,
+                            textRes = R.string.restore_purchases,
+                            onClick = actionListener::onRestorePurchases
                         )
-                        if(hasActiveSubscription) {
-                            FudgeTvButton(
-                                type = FudgeTvButtonTypeEnum.LARGE,
-                                style = FudgeTvButtonStyleTypeEnum.INVERSE,
-                                textRes = R.string.restore_purchases,
-                                onClick = actionListener::onRestorePurchases
-                            )
-                        }
                     }
                 }
-            }
+            )
         }
     }
 }
